12cb6a3c1c74a8ba052a80b1dbf8fade6061ff3a
Rahul Singh <thesincererahul@gmail.com>
2024-12-19 17:13:05 +0530

    component intro

---
+++ 2_FirstViteProject/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+// import MyButton from './MyButton';
+import Bt from './MyButton';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Bt />
+      <Bt />
+      <Bt />
+    </div>
+  );
+}
+
+// Button Component
+// eslint: If you meant to render a React component, start its name with an uppercase letter.
+// function MyButton() {
+//   return <button>Click</button>
+// }
+
+export default App
---
+++ 2_FirstViteProject/src/MyButton.jsx
@@ -0,0 +1,7 @@
+// Button Component
+// eslint: If you meant to render a React component, start its name with an uppercase letter.
+function MyButton() {
+    return <button>Click</button>
+}
+
+export default MyButton;
