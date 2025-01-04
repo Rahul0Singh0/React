892efef3c6153fdf626bf8d8f4483414f6b116ad
Rahul Singh <thesincererahul@gmail.com>
2025-01-04 18:28:24 +0530

    update code and note

--- 7_Reconcillation/src/App.jsx
+++ 7_Reconcillation/src/App.jsx
@@ -1,14 +1,23 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 
 function Input({ type, placeholder }) {
-  return <input type={type} placeholder={placeholder} />
+  const [s, setS] = useState('');
+  return <input type={type} placeholder={placeholder} value={s} onChange={(e) => setS(e.target.value)} />
 }
 
 function App() {
 
   const [isStudent, setIsStudent] = useState(false);
-  console.log(React.createElement(Input, null, 'Hello World'));
+  const [count, setCount] = useState(0); // here state update - re-rendering
+
+  function updateCount() { // count: 0 -> 1
+    // setCount(count + 1); // setCount(0 + 1)
+    // setCount(count + 1); // setCount(0 + 1)
+    setCount((prevCount) => prevCount + 1); // setCount(0 + 1); // [cb]
+    setCount((prevCount) => prevCount + 1); // setCount(1 + 1); // [cb]
+    console.log(count); // current render
+  }
 
   return (
     <>
@@ -20,9 +29,16 @@ function App() {
         }} />
         <label htmlFor='student'>Are you a student?</label>
         <br/>
-        { isStudent ? <Input type={"text"} placeholder={"Enter your school name"} />
-          : <Input type={"text"} placeholder={"Enter you company name"} /> }
+        {/* { isStudent ? <Input type={"text"} placeholder={"Enter your school name"} /> : null }
+        { !isStudent ? <Input type={"text"} placeholder={"Enter you company name"} /> : null } */}
+        {isStudent ? <Input type={"text"} placeholder={"Enter your school name"} key={"school"} /> 
+        : <Input type={"text"} placeholder={"Enter you company name"} key={"company"} /> }
       </form>
+      <div>
+        Count Value: {count}
+        <br/>
+        <button onClick={updateCount}>Click</button>
+      </div>
     </>
   )
 }
